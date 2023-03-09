176dbd9e43ebc0bbc946139bb82e85ea70ce94e1
Elvi <elvielvis23@gmail.com>
2023-03-09 18:12:37 +0100

    Adoptation Component

--- src/Components/Adoptation/Adoptation.js
+++ src/Components/Adoptation/Adoptation.js
@@ -8,22 +8,27 @@ const Adoptation = () =>{
     return (
         <>
 
-        {Popup ? 
-        <div className="PopUp"> 
-            <div className="UserData">
-                <input type="text" placeholder="Name"/>
-                <input type="text" placeholder="Lastname"/>
-                <input type="text" placeholder="Email"/>
-                <input type="text" placeholder="Enter Your Location"/>
-                <input type="number" placeholder="Age"/>
-            </div>
-                <button className="ClosePopup" onClick={() => setPopup(false)}>Cancel</button>    
-                <button type="submit" className="Adopt">Adopt</button>
-        </div> 
-        : <> 
+
+        <div> 
             <button onClick={() => setPopup(true)} className="AdoptBtn">Adopt</button> 
             <p>Adopt Now!</p>
-        </>}
+        </div>    
+            {Popup ? 
+            <div className="PopUp"> 
+                <div className="UserData">
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Lastname"/>
+                    <input type="text" placeholder="Email"/>
+                    <input type="text" placeholder="Enter Your Location"/>
+                    <input type="number" placeholder="Age"/>
+                    <div className="buttons">
+                        <button className="ClosePopup" onClick={() => setPopup(false)}>Cancel</button>    
+                        <button type="submit" className="Adopt">Adopt</button>
+                    </div>
+                </div>    
+            </div> 
+            : ""}
+
         </>
     )
 }
