bcce804a90ff411e909e93f4e18d8a6bef81c7ed
BekimDajlani <bekim_dajlani@yahoo.com>
2023-03-15 15:21:37 +0100

    commit to update changes

--- src/Components/AddCategoryPopup.js
+++ src/Components/AddCategoryPopup.js
@@ -2,9 +2,6 @@ import { useState } from 'react';
 import './singleCategory.scss';
 const AddCategoryPopup = (props) => {
 
-<<<<<<< HEAD
-    const [userInput,setUserInput] = useState('');
-=======
     const [userInput, setUserInput] = useState('');
 
     const handleUserInput = (e) => {
@@ -15,11 +12,8 @@ const AddCategoryPopup = (props) => {
         props.addCategory(userInput);
         props.showPopup();
     };
->>>>>>> a9f142f346ed0d3a72d437cb9607ac9c9c5394a9
 
-    const handleUserInput = (e) => {
-        setUserInput(e.target.value)
-    };
+ 
 
     console.log(userInput);
     
@@ -27,14 +21,6 @@ const AddCategoryPopup = (props) => {
         <div className="popup">
             <h3>Select new category</h3>
             <select value={userInput} onChange={handleUserInput}>
-<<<<<<< HEAD
-                <option value='Squirrel'>Squirrel</option>
-                <option value='Fish'>Fish</option>
-                <option value='Horse'>Horse</option>
-                <option value='Snakes'>Snakes</option>
-            </select>
-            <button type='submit' className='add-category' >Submit</button>
-=======
                 <option>Select Category</option>
                 <option>Squirrel</option>
                 <option>Fish</option>
@@ -42,7 +28,6 @@ const AddCategoryPopup = (props) => {
                 <option>Snakes</option>
             </select>
             <button className='add-category' onClick={handleCategories}>Submit</button>
->>>>>>> a9f142f346ed0d3a72d437cb9607ac9c9c5394a9
         </div>
     )
 }
