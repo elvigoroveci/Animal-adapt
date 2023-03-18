d2528727c67edf158d43db0089958c1ed2745fac
jurgenhideni97 <hidenijurgen@gmail.com>
2023-03-18 12:04:16 +0100

    edited loginPage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import LoginPage from "./components/LoginPage/LoginPage";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "../LoginPage/LoginPage.scss";
 import facebookImg from "../../images/facebook.png";
 import googleImg from "../../images/google-icon.png";
@@ -6,28 +8,10 @@ import vkSocial from "../../images/vk-social.png";
 import dogImg from "../../images/dog-img.jpg";
 
 const LoginPage = () => {
-  const handleSignUp = () => {
-    const inputFields = document.querySelectorAll(".input-group input");
-    let hasEmptyFields = false;
+  const [password, setPassword] = useState("");
 
-    inputFields.forEach((input) => {
-      if (input.value === "") {
-        hasEmptyFields = true;
-      }
-    });
-
-    const passwordInput = document.querySelector('input[type="password"]');
-    const passwordPattern = /^(?=.*?[^\w\s]).{8,}$/;
-
-    if (hasEmptyFields) {
-      alert("Please fill in all the required fields");
-    } else if (!passwordPattern.test(passwordInput.value)) {
-      alert(
-        "Password must be at least 8 characters long and contain a special character"
-      );
-    } else {
-      alert("Sign up successful!");
-    }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   return (
@@ -36,36 +20,66 @@ const LoginPage = () => {
         <div className="signup">
           <h1 className="header">Create account</h1>
           <div className="socials">
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="http://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={facebookImg} alt="facebook icon" />
             </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="http://mail.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={googleImg} alt="facebook icon" />
             </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="http://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={twitterImg} alt="facebook icon" />
             </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a href="http://vk.com" target="_blank" rel="noopener noreferrer">
               <img src={vkSocial} alt="vk social icon" />
             </a>
           </div>
-          <div className="input-group">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="email" placeholder="E-Mail" />
-            <input type="number" placeholder="Age" />
-            <input type="tel" placeholder="Phone Number" />
-            <input type="text" placeholder="City" />
-            <input type="text" placeholder="Adress" />
-            <input type="password" placeholder="Password" />
-          </div>
-          <h2>
-            <input className="checkbox" type="checkbox" />I agree to{""}
-            <span>Terms</span> and{""} <span>Privacy Policy</span>
-          </h2>
-          <button className="btn" type="submit" onClick={handleSignUp}>
-            Sign Up
-          </button>
+          <form>
+            <div className="input-group">
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Last Name" />
+              <input type="email" placeholder="E-Mail" />
+              <input type="number" placeholder="Age" />
+              <input type="tel" placeholder="Phone Number" />
+              <input type="text" placeholder="City" />
+              <input type="text" placeholder="Adress" />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              {password.length < 8 && (
+                <p className="error">
+                  *Password must contain at least 8 characters.
+                </p>
+              )}
+            </div>
+            <h2>
+              <input className="checkbox" type="checkbox" />I agree to{""}
+              <span>Terms</span> and{""} <span>Privacy Policy</span>
+            </h2>
+            <div className="buttons">
+              <button className="btn" type="submit">
+                Sign Up
+              </button>
+              <h3>or</h3>
+              <button className="btn" type="submit">
+                Log in to existing account
+              </button>
+            </div>
+          </form>
         </div>
         <img src={dogImg} alt="dog-img" />
       </div>
