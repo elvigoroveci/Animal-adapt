995f6b2c74153b92e47aff29b5207a9f25d2c5fd
Bekim Dajlani <bekim_dajlani@yahoo.com>
2023-03-18 15:55:07 +0100

    adding styles after merge

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter className="body-container">
+    <div className="body-container">
+    <BrowserRouter >
       <Routes>
         <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
