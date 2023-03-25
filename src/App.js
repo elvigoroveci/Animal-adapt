c711a92b3b82d3cdc922538589e1072ab588bfae
Elvi <elvielvis23@gmail.com>
2023-03-25 12:38:36 +0100

    Router Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categories from "./Components/Categories";
 import Adoptation from "./Components/Adoptation/Adoptation";
+import SingleAnimalPage from './Components/SingleAnimalPage/SingleAnimalPage';
+import MenuMain from "./Navbar-Wrapper/MenuMain";
 
 
 function App() {
   return (
     <div className="body-container">
-    <BrowserRouter >
-      <Routes>
+      <BrowserRouter >
+       <Routes>
+        <Route index element={<Adoptation />} />
+        <Route path="/" element={<MenuMain/>} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/animals/:id" element={<SingleAnimalPage />} />       
         <Route path="/:petsId" element={ <Adoptation/>} />
-      </Routes>
-    </BrowserRouter>
+       </Routes>
+     </BrowserRouter>
     </div>
   );
 }
